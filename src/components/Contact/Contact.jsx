14828601b4ad4d908bc0dd737f3063b8ba14ed4e
Shoomankhatri <shoomankhatri@gmail.com>
2024-01-15 17:39:02 +0545

    edited

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
             <h1>Contact Us</h1>
             <p className="heading">Reach out to us on our social media profiles:</p>
             <div className="social-links">
-                <a href="https://www.facebook.com/your-facebook-page"><i className="fa-brands fa-facebook-f fa-2xl"></i></a>
+                <a href="https://www.facebook.com/Alam909"><i className="fa-brands fa-facebook-f fa-2xl" style={{ color: '#316FF6' }}></i></a>
                 <a href="https://www.instagram.com/your-instagram-page"><i className="fa-brands fa-instagram fa-2xl" style={{ color: '#f2075a' }}></i></a>
                 <a href="https://www.twitter.com/your-twitter-page"><i className="fa-brands fa-twitter fa-2xl" style={{ color: '#0f0f0f' }}></i></a>
                 <a href="https://www.linkedin.com/your-linkedin-page"><i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#255ab6' }}></i></a>
@@ -15,7 +15,7 @@ const Contact = () => {
             </div>
             <br /><br />
             <p>
-                <h3 className="social-links">Phone No: + 977 9856789415</h3>
+                <h3 className="social-links">Phone No: + 977 9867452183</h3>
             </p>
         </div>
     );
